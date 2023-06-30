5a9bbe4aea47c3568e82d885a144427c05f2ce8b
Lama Koleib <lkole095@uottawa.ca>
2023-06-30 01:12:38 -0400

    Added bootstrap and fixed minor issues

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,25 @@ const  Home = () => {
                 <p className="intro-text">Indulge in a world of flavors, where creativity knows no bounds. From classic favorites to unique and innovative creations, our talented cake artisans bring their expertise and passion to every masterpiece they create.</p>
                 <button className="discover-btn">Discover Cakes</button>
             </div>
-            <div className="image-bg"></div>
+            
+
+            <div className="info-1">
+                <div class="text-center">
+                    <h1 className="who">
+                            Who are we?
+                    </h1>
+                    <p className="who-p-1">
+                        At Dreamy Delights, we believe that every occasion deserves a delectable treat. Whether you're celebrating a birthday, anniversary, graduation, or simply indulging in a sweet craving, our diverse selection of cakes is here to make every moment memorable.
+
+                    </p>
+
+                    <p className="who-p-2">
+                        With attention to detail and a commitment to quality, we ensure that every cake that leaves our bakery is a true delight. Our passion for perfection drives us to use only the finest ingredients, ensuring that each bite is a moment of pure bliss.
+
+                    </p>
+                </div>
+                
+            </div>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,54 @@
 const Navbar = () =>{
     return(
-        
-        <nav className="navbar">
 
-        <h1 className="nav-title">Dreamy Delights</h1>
-            <div className="links">
-                <a href="/">HOME</a>
-                <a href="/about">ABOUT</a>
-                <a href="/shop">CAKES</a>
-                <a href="/gallery">GALLERY</a>
-                <a href="/contact">CONTACT US</a>
+        <div className="nav-content">
+            <nav class="navbar navbar-expand-md fixed-top navbar-light">
+        <div class="container-xxl">
+            <a href="#landing" class="navbar-brand">
+                <span class="fw-bold nav-title ">Dreamy Delights
+            </span>
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            
+
+            <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#landing" class="nav-link">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#our-doctors" class="nav-link">CAKES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#services" class="nav-link">GALLERY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#contact-us" class="nav-link">CONTACT US</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
+        </div>
+        
+        // <nav className="navbar">
+
+        // <h1 className="nav-title">Dreamy Delights</h1>
+        //     <div className="links">
+        //         <a href="/">HOME</a>
+        //         <a href="/about">ABOUT</a>
+        //         <a href="/shop">CAKES</a>
+        //         <a href="/gallery">GALLERY</a>
+        //         <a href="/contact">CONTACT US</a>
+        //     </div>
+        // </nav>
 
       
 
